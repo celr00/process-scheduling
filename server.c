f2f44531131d1da389589af7271e4980028d070d
KarenNR <74948680+KarenNR@users.noreply.github.com>
2024-05-03 23:37:52 -0600

    SRT algorithm and changed queue condition for other algorithms

--- server.c
+++ server.c
@@ -30,6 +30,7 @@ int globalID = 0;
 bool executing = false;
 pid_t wpid;
 int status = 0;
+int currentEventId = -1;
 // Quantum para Round Robin
 int quantum = 5;
 
@@ -93,12 +94,29 @@ bool is_queue_empty()
         return false;
 }
 
+// Regresar posición de trabajo con menor tiempo de ejecución
+int getShortestJob()
+{
+    int n = get_queue_length();
+    int minIndex = -1;
+    int minTime = 100000;
+    for (int i = 0; i < n; i++)
+    {
+        if (procesos[i].remaining_time < minTime)
+        {
+            minIndex = i;
+            minTime = procesos[i].remaining_time;
+        }
+    }
+    return minIndex;
+}
+
 // RECIBIR EVENTOS DEL CLIENTE
 void funcion_usr1(int sig, siginfo_t *info, void *secret)
 {
-    printf("Señal recibida de %d. Creando proceso con ID %d\n", getpgid(info->si_pid), globalID);
     int random_number = (rand() % 20) + 1; // Duración aleatoria
     Evento event = {globalID, random_number, random_number, 0, 0, 0};
+    printf("Señal recibida de %d. Creando proceso con ID %d (Burst Time: %d s)\n", getpgid(info->si_pid), globalID, random_number);
     enqueue(event);
     globalID++;
 }
@@ -106,144 +124,136 @@ void funcion_usr1(int sig, siginfo_t *info, void *secret)
 // FIRST COME FIRST SERVE
 void fcfs()
 {
-    if (!is_queue_empty())
+    if (!executing)
     {
-        if (!executing)
+        print_queue();
+        Evento event = dequeue(0);
+        printf("FCFS: Ejecutar evento %d en %d segundos\n", event.id, event.burst_time);
+        pid_t pid = fork();
+        executing = true;
+        if (pid == 0)
         {
-            print_queue();
-            Evento event = dequeue(0);
-            printf("FCFS: Ejecutar evento %d en %d segundos\n", event.id, event.burst_time);
-            pid_t pid = fork();
-            executing = true;
-            if (pid == 0)
-            {
-                sleep(event.burst_time);
-                printf("Evento %d terminado\n", event.id);
-                exit(0);
-            }
+            sleep(event.burst_time);
+            printf("Evento %d terminado\n", event.id);
+            exit(0);
         }
-    }
-    else
-    {
-        sleep(5);
     }
 }
 
 // FIRST IN FIRST OUT
 void fifo()
 {
-    if (!is_queue_empty())
+    if (!executing)
     {
-        if (!executing)
+        print_queue();
+        Evento event = dequeue(0);
+        printf("FIFO: Ejecutar evento %d en %d segundos\n", event.id, event.burst_time);
+        pid_t pid = fork();
+        executing = true;
+        if (pid == 0)
         {
-            print_queue();
-            Evento event = dequeue(0);
-            printf("FIFO: Ejecutar evento %d en %d segundos\n", event.id, event.burst_time);
-            pid_t pid = fork();
-            executing = true;
-            if (pid == 0)
-            {
-                sleep(event.burst_time);
-                printf("Evento %d terminado\n", event.id);
-                exit(0);
-            }
+            sleep(event.burst_time);
+            printf("Evento %d terminado\n", event.id);
+            exit(0);
         }
-    }
-    else
-    {
-        sleep(5);
     }
 }
 
 // ROUND ROBIN
 void round_robin()
 {
-    if (!is_queue_empty())
+    if (!executing)
     {
-        if (!executing)
+        print_queue();
+        // Seleccionar primer evento
+        Evento evento = dequeue(0);
+        // Asignar quantum o tiempo restante del proceso
+        int execute_time = (evento.remaining_time > quantum) ? quantum : evento.remaining_time;
+        // Actualizar tiempo restante del proceso
+        evento.remaining_time -= execute_time;
+        // Ejecutar parte del proceso
+        printf("RR: Ejecutar evento %d en %d segundos\n", evento.id, execute_time);
+        pid_t pid = fork();
+        executing = true;
+        if (pid == 0)
         {
-            print_queue();
-            // Seleccionar primer evento
-            Evento evento = dequeue(0);
-            // Asignar quantum o tiempo restante del proceso
-            int execute_time = (evento.remaining_time > quantum) ? quantum : evento.remaining_time;
-            // Actualizar tiempo restante del proceso
-            evento.remaining_time -= execute_time;
-            // Ejecutar parte del proceso
-            printf("RR: Ejecutar evento %d en %d segundos\n", evento.id, execute_time);
-            pid_t pid = fork();
-            executing = true;
-            if (pid == 0)
-            {
-                sleep(execute_time);
-                printf("Quantum de evento %d terminado, quedan %d segundos\n", evento.id, evento.remaining_time);
-                exit(0);
-            }
-            while ((wpid = wait(&status)) > 0)
-                ; // Esperar ejecución de evento
-            // Volver a meter evento si aún no se termina
-            if (evento.remaining_time > 0)
-            {
-                printf("Agregando evento %d nuevamente\n", evento.id);
-                enqueue(evento);
-            }
-            else
-            {
-                printf("Evento %d terminado\n", evento.id);
-            }
+            sleep(execute_time);
+            printf("Quantum de evento %d terminado, quedan %d segundos\n", evento.id, evento.remaining_time);
+            exit(0);
         }
-    }
-    else
-    {
-        sleep(5);
+        while ((wpid = wait(&status)) > 0)
+            ; // Esperar ejecución de evento
+        // Volver a meter evento si aún no se termina
+        if (evento.remaining_time > 0)
+        {
+            printf("Agregando evento %d nuevamente\n", evento.id);
+            enqueue(evento);
+        }
+        else
+        {
+            printf("Evento %d terminado\n", evento.id);
+        }
     }
 }
 
 // SHORTEST JOB FIRST
 void sjf()
 {
-    if (!is_queue_empty())
+    if (!executing)
     {
-        if (!executing)
+        print_queue();
+        // Encontrar trabajo más corto
+        int minIndex = getShortestJob();
+        Evento event = procesos[minIndex];
+        printf("SJF: Ejecutar evento %d en %d segundos\n", event.id, event.burst_time);
+        pid_t pid = fork();
+        executing = true;
+        if (pid == 0)
         {
-            print_queue();
-            // Encontrar trabajo más corto
-            int n = get_queue_length();
-            int minIndex = -1;
-            int minTime = 100000;
-            for (int i = 0; i < n; i++)
-            {
-                if (procesos[i].burst_time < minTime)
-                {
-                    minIndex = i;
-                    minTime = procesos[i].burst_time;
-                }
-            }
-            Evento event = procesos[minIndex];
-            printf("SJF: Ejecutar evento %d en %d segundos\n", event.id, event.burst_time);
-            pid_t pid = fork();
-            executing = true;
-            if (pid == 0)
-            {
-                sleep(event.burst_time);
-                printf("Evento %d terminado\n", event.id);
-                exit(0);
-            }
-            while ((wpid = wait(&status)) > 0)
-                ; // Esperar ejecución de evento
-            dequeue(minIndex);
+            sleep(event.burst_time);
+            printf("Evento %d terminado\n", event.id);
+            exit(0);
         }
-    }
-    else
-    {
-        sleep(5);
+        while ((wpid = wait(&status)) > 0)
+            ; // Esperar ejecución de evento
+        dequeue(minIndex);
     }
 }
 
 // SHORTEST REMAINING TIME
 void srt()
 {
-    printf("Shortest Remaining Time\n");
+    if (!executing)
+    {
+        // print_queue();
+        // Obtener trabajo más corto
+        int minIndex = getShortestJob();
+        // printf("Mínimo índice actual %d\n", minIndex);
+        Evento *evento = &procesos[minIndex]; // Apuntar a arreglo de procesos para poder actualizarlo posteriormente
+        if (currentEventId != evento->id)
+        {
+            currentEventId = evento->id;
+            print_queue();
+            printf("SRT: Cambiando a proceso %d con tiempo de ejecución: %d\n", evento->id, evento->remaining_time);
+        }
+        pid_t pid = fork();
+        executing = true;
+        if (pid == 0)
+        {
+            sleep(1);
+            exit(0);
+        }
+        while ((wpid = wait(&status)) > 0)
+            ; // Esperar ejecución de evento
+        // Actualizar tiempo de evento
+        evento->remaining_time--;
+        // Eliminar evento si ya terminó
+        if (evento->remaining_time <= 0)
+        {
+            printf("Evento %d terminado\n", evento->id);
+            dequeue(minIndex);
+        }
+    }
 }
 
 // HIGHEST RESPONSE-RATIO NEXT
@@ -285,8 +295,8 @@ int main()
     // Para pruebas
     /* Evento eventos[] = {
         {1, 5, 5, 0, 0, 0},
-        {2, 1, 1, 0, 0, 0},
-        {3, 2, 2, 0, 0, 0},
+        {2, 3, 3, 0, 0, 0},
+        {3, 4, 4, 0, 0, 0},
         {4, 6, 6, 0, 0, 0}
     };
     enqueue(eventos[0]);
@@ -296,9 +306,17 @@ int main()
 
     while (1)
     {
-        // fcfs();
-        // fifo();
-        // sjf();
-        round_robin();
+        if (!is_queue_empty())
+        {
+            // fcfs();
+            // fifo();
+            // sjf();
+            // round_robin();
+            srt();
+        }
+        else
+        {
+            sleep(5);
+        }
     }
 }
