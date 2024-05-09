e9349e70cba24172baab595fb622c5b92c97fe1d
KarenNR <74948680+KarenNR@users.noreply.github.com>
2024-05-09 10:58:26 -0600

    Basic structure for client

--- cliente.py
+++ cliente.py
@@ -1,11 +1,16 @@
 import tkinter as tk
 import os
+import signal
+import sys
 
-server_pid = 4087
+def signal_handler(sig, frame):
+    label1.config(text="Estatus: EJECUTANDO")
 
 def send_hup():
+    server_pid = server_pid_txt.get()
     os.system("kill -s HUP {}".format(server_pid))
 
+# TO EXIT: sys.exit(0)
 
 window = tk.Tk()
 window.title("Cliente con PID {}".format(os.getpid()))
@@ -23,7 +28,15 @@ posicion_y = (alto_pantalla - alto_window) // 2
 # Establecer el tamaño y la posición de la window
 window.geometry(f"{ancho_window}x{alto_window}+{posicion_x}+{posicion_y}")
 
+server_pid_txt = tk.Entry(window, width=30)
+server_pid_txt.pack()
+
 btn = tk.Button(window, text='SIGHUP', command=send_hup)
 btn.pack()
+
+label1 = tk.Label(window, text="Estatus: ")
+label1.pack()
+
+signal.signal(signal.SIGUSR2, signal_handler)
 
 window.mainloop()
